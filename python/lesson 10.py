1d9f81ed6fda871bb0fd9d446755c89d46ce483e
codingal12 <sudhajoshi093@gmail.com>
2024-12-24 21:51:44 +0530

    Add files via upload

---
+++ python/lesson 10.py
@@ -0,0 +1,8 @@
+###Write a program to calculate the sum of whole numbers.
+
+num=int(input("enter the sum of numbers you want"))
+sum=0
+
+for v in range(1,num+1):
+    sum =sum+v 
+    print("The sum is :",sum)
